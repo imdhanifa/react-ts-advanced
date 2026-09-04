d0ff605a1f4180ec4f1111b30d1cf7ad45afe4a4
Hanifa <imdhanifa@gmail.com>
2026-09-04 18:11:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-ts-advanced/',
   server: {
     host: true,
   }
